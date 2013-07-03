1e520ead29e6a68e3e05fa8968d836a46b6c25d1
Juan <juan@gjtech.net>
2013-07-03 10:33:41 -0700

    Commit

--- cronlogger.sh
+++ cronlogger.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 #
-# cronLogger.sh: Function to send custom logs to syslog
+# cronLogger.sh: Plugin to send custom logs to syslog
 #
 # Usage:
 #		cronLogger "Message that I want to send to syslog"
 
 function cronLogger()
 {
-	[ -z "$1" ] && exit 0
-	cronMessage="$1"
+	[ -z "$1" ] && exit 0;
+	cronMessage="$1";
 
-	tty -s
-	crontask=$?
+	tty -s;
+	crontask=$?;
 
-	[ $crontask -gt 0 ] && /usr/bin/logger -i -p cron.info -t CRON "${cronMessage}"
-	return
+	[ $crontask -gt 0 ] && /usr/bin/logger -i -p cron.info -t CRON "${cronMessage}";
+	return;
 }
+
+##
+## EOF
+##
